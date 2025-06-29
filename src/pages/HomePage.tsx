aabe91760e7ff51162d767c2927cf11b852e15b9
thecustomsoundarchitect <thecustomsoundarchitect@gmail.com>
2025-06-29 05:23:37 -0400

    Remove icons from homepage buttons

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -42,7 +42,6 @@ export default function HomePage() {
               <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                 <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-                  <Plus className="w-4 h-4 mr-2" />
                   Create New Soul Hug
                 </span>
               </button>
@@ -52,7 +51,6 @@ export default function HomePage() {
               <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                 <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-                  <Eye className="w-4 h-4 mr-2" />
                   View My Soul Hugs
                 </span>
               </button>
